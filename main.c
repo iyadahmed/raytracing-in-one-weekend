7311d625cc29fcf14cac3075210555ed7d39f8b8
Iyad Ahmed <cgonfire@outlook.com>
2021-05-12 22:44:29 +0200

    More render progress

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "ppmio.h"
 #include "vec3.h"
-//#include "ray.h"
+#include "ray.h"
 
 
 //color ray_color(const ray& r) {
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 {
 	// Image
 	const float aspect_ratio = 16.0 / 9.0;
-    const int image_width = 1024;
+    const int image_width = 400;
     const int image_height = (int)(image_width / aspect_ratio);
     
     // Camera
@@ -32,11 +32,14 @@ int main(int argc, char *argv[])
     const float viewport_width = aspect_ratio * viewport_height;
     const float focal_length = 1.0;
 
-    point3 origin = {0, 0, 0};
-    vec3 horizontal = {viewport_width, 0, 0};
-    vec3 vertical = {0, viewport_height, 0};
-    // TODO: unuglify this
-    double * lower_left_corner = sub(add(add(origin, scale(horizontal, -0.5)), scale(vertical, -0.5)), (vec3){0, 0, focal_length});
+    point3 origin = vec3_create(0, 0, 0);
+    vec3 horizontal = vec3_create(0, 0, 0);
+    vec3 vertical = vec3_create(0, viewport_height, 0);
+
+    vec3 lower_left_corner = vec3_create(0,0,0);
+    lower_left_corner[0] = origin[0] - horizontal[0]/2 - vertical[0]/2;
+    lower_left_corner[1] = origin[1] - horizontal[1]/2 - vertical[1]/2;
+    lower_left_corner[2] = origin[2] - horizontal[2]/2 - vertical[2]/2 - focal_length;
 	
 
 	unsigned char *buf = (unsigned char *)malloc(image_width * image_height * 3);
@@ -52,18 +55,26 @@ int main(int argc, char *argv[])
 		for (i = 0; i < image_width; i++)
 		{
 			// TODO: Render Code
-			double r = (double)i / image_width;
-			double g = (double)j / image_height;
+			double u = (double)i / image_width;
+			double v = (double)j / image_height;
+			
+			ray r;
+			r.orig = origin;
+			r.dir = vec3_create(0,0,0);
+			r.dir[0] = lower_left_corner[0] + u*horizontal[0] + v*vertical[0] - origin[0];
+			r.dir[1] = lower_left_corner[1] + u*horizontal[1] + v*vertical[1] - origin[1];
+			r.dir[2] = lower_left_corner[2] + u*horizontal[2] + v*vertical[2] - origin[2];
 			double b = .25;
-			write_color(tmp_buf, r, g, b);
+			write_color(tmp_buf, u, v, 0);
 			tmp_buf += 3;
 		}
 		time += (double)(clock() - begin) / CLOCKS_PER_SEC;
 	}
 	printf("\nDone.\n");
 	printf("Render Time: %f\n", time);
+	printf("Exporting PPM ...\n");
 	write_ppm(buf, image_width, image_height);
+	printf("Done.\n");
 	free(buf);
-	
 	return 0;
 }
--- vec3.h
+++ vec3.h
@@ -5,20 +5,29 @@
 
 // TODO: vec3 struct
 
-typedef double vec3[3];
+typedef double * vec3;
 typedef vec3 color;
 typedef vec3 point3;
 
-double * add(vec3 a, vec3 b)
+vec3 vec3_create(double x, double y, double z)
 {
-	static vec3 out;
+	vec3 out = malloc(sizeof(double) * 3);
+	out[0] = x;
+	out[1] = y;
+	out[2] = z;
+	return out;
+}
+
+vec3 add(vec3 a, vec3 b)
+{
+	vec3 out = malloc(sizeof(double) * 3);
 	out[0] = a[0] + b[0];
 	out[1] = a[1] + b[1];
 	out[2] = a[2] + b[2];
 	return out;
 }
 
-double * sub(vec3 a, vec3 b)
+vec3 sub(vec3 a, vec3 b)
 {
 	static vec3 out;
 	out[0] = a[0] - b[0];
@@ -27,9 +36,9 @@ double * sub(vec3 a, vec3 b)
 	return out;
 }
 
-double * mul(vec3 a, vec3 b)
+vec3 mul(vec3 a, vec3 b)
 {
-	static vec3 out;
+	vec3 out = malloc(sizeof(double) * 3);
 	out[0] = a[0] * b[0];
 	out[1] = a[1] * b[1];
 	out[2] = a[2] * b[2];
@@ -51,9 +60,9 @@ double dot(vec3 a, vec3 b)
 	return a[0]*b[0] + a[1]*b[1] + a[2]*b[2];
 }
 
-double * scale(vec3 e, double s)
+vec3 scale(vec3 e, double s)
 {
-	static vec3 out;
+	vec3 out = malloc(sizeof(double) * 3);
 	out[0] = e[0] * s;
 	out[1] = e[1] * s;
 	out[2] = e[2] * s;
@@ -67,7 +76,7 @@ double * normalize(vec3 e)
 
 double * cross(const vec3 u, const vec3 v)
 {
-	static vec3 out;
+	vec3 out = malloc(sizeof(double) * 3);
 	out[0] = u[1] * v[2] - u[2] * v[1];
 	out[1] = u[2] * v[0] - u[0] * v[2];
 	out[2] = u[0] * v[1] - u[1] * v[0];
