b1265c2725a246f07f95e455147667e55b454b49
Iyad Ahmed <cgonfire@outlook.com>
2021-05-12 09:24:29 +0200

    Write multidim data to dynamic memory

--- main.c
+++ main.c
@@ -2,22 +2,30 @@
 #include <stdlib.h>
 #include "ppmio.h"
 
+int offset(int i, int j, int k, int width, int height) { 
+    return (i * width * 3) + (j * 3) + k;
+}
 
 int main(int argc, char *argv[]) {
-	unsigned int width = 256;
-	unsigned int height = 256;
+	unsigned int width = 1024;
+	unsigned int height = 1024;
 	// Create and fill data
-	unsigned char data[width][height][3];
+	unsigned char *data = (unsigned char *)malloc(sizeof(unsigned char) * width * height * 3);
 	int i, j, k;
-	for (j=height-1; j>=0; --j)
+	for (i=0; i<width; i++)
 	{
-		for (i=width-1; i>=0; --i)
+		for (j=0; j<height; j++)
 		{
-			data[i][j][0] = (unsigned char)(((float)j / height) * 255);
-			data[i][j][1] = (unsigned char)(((float)i / width) * 255);
-			data[i][j][2] = 0;
+//			*data = (unsigned char)(((float)i / height) * 255);
+//			*(data+1) = (unsigned char)(((float)j / height) * 255);
+//			*(data+2) = 0;
+//			data += 3;
+			data[offset(i, j, 0, width, height)] = (unsigned char)(((float)i / height) * 255);
+			data[offset(i, j, 1, width, height)] = (unsigned char)(((float)j / width) * 255);
+			data[offset(i, j, 2, width, height)] = 0;
 		}
 	}
 	write_ppm(data, width, height);
+	free(data);
 	return 0;
 }
