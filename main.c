b49bfa2fa961a5dc499d38d8db43113d4e0cd468
Iyad Ahmed <cgonfire@outlook.com>
2021-05-12 12:43:57 +0200

    Report render status

--- main.c
+++ main.c
@@ -2,26 +2,29 @@
 #include <stdlib.h>
 #include "ppmio.h"
 
+
 int main(int argc, char *argv[])
 {
 	unsigned int width = 1024;
 	unsigned int height = 512;
 	unsigned char *data = (unsigned char *)malloc(width * height * 3);
-	unsigned char *orig_data = data;
-	int i, j, k;
-	int foo = 0;
+	unsigned char *tmp_data = data;
+	int i, j;
 	for (j = height-1; j >= 0; j--)
 	{
+		printf("\rScanlines remaining: %d", j);
+		fflush(stdout);
 		for (i = 0; i < width; i++)
 		{
 			// TODO: Render Code
-			*data = (unsigned char)(((float)i / width) * 255);
-			*(data+1) = (unsigned char)(((float)j / height) * 255);
-			*(data+2) = 127;
-			data += 3;
+			*tmp_data = (unsigned char)(((float)i / width) * 255);
+			*(tmp_data+1) = (unsigned char)(((float)j / height) * 255);
+			*(tmp_data+2) = 127;
+			tmp_data += 3;
 		}
 	}
-	write_ppm(orig_data, width, height);
-	free(orig_data);
+	write_ppm(data, width, height);
+	free(data);
+	printf("\nDone.\n");
 	return 0;
 }
