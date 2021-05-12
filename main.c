63172bb7b6c42c4fcc5a782139a383ed1dd88fa0
Iyad Ahmed <cgonfire@outlook.com>
2021-05-12 09:41:17 +0200

    Better performance, pointer arthimetic instead of offset calculation

--- main.c
+++ main.c
@@ -2,27 +2,21 @@
 #include <stdlib.h>
 #include "ppmio.h"
 
-int offset(int i, int j, int k, int width, int height) { 
-    return (i * width * 3) + (j * 3) + k;
-}
-
 int main(int argc, char *argv[]) {
-	unsigned int width = 1024;
-	unsigned int height = 1024;
+	unsigned int width = 2048;
+	unsigned int height = 2048;
 	// Create and fill data
 	unsigned char *data = (unsigned char *)malloc(sizeof(unsigned char) * width * height * 3);
+	unsigned char *tmp_data = data;
 	int i, j, k;
 	for (i=0; i<width; i++)
 	{
 		for (j=0; j<height; j++)
 		{
-//			*data = (unsigned char)(((float)i / height) * 255);
-//			*(data+1) = (unsigned char)(((float)j / height) * 255);
-//			*(data+2) = 0;
-//			data += 3;
-			data[offset(i, j, 0, width, height)] = (unsigned char)(((float)i / height) * 255);
-			data[offset(i, j, 1, width, height)] = (unsigned char)(((float)j / width) * 255);
-			data[offset(i, j, 2, width, height)] = 0;
+			*tmp_data = (unsigned char)(((float)i / height) * 255);
+			*(tmp_data+1) = (unsigned char)(((float)j / height) * 255);
+			*(tmp_data+2) = 0;
+			tmp_data += 3;
 		}
 	}
 	write_ppm(data, width, height);
