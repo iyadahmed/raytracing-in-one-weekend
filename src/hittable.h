29f3e6b644777723c0df5af6d039a88017a59291
Iyad Ahmed <iyadahmed@cgonfire.com>
2021-11-21 22:57:35 +0200

    Split hittable to source and header files

--- src/hittable.h
+++ src/hittable.h
@@ -1,26 +1,13 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
-#include "ray.h"
 #include "vec3.h"
 
-typedef struct HitRecord {
+typedef struct HitRecord
+{
     Point3 position;
     Vec3 normal;
     int is_front_face;
 } HitRecord;
-
-void set_hit_record_face_normal(HitRecord *hit_record, Ray *ray, Vec3 *outward_normal) {
-    hit_record->is_front_face = v3_dot(&ray->direction, outward_normal) < 0;
-    if (hit_record->is_front_face) {
-        hit_record->normal.x = outward_normal->x;
-        hit_record->normal.y = outward_normal->y;
-        hit_record->normal.z = outward_normal->z;
-    } else {
-        hit_record->normal.x = -outward_normal->x;
-        hit_record->normal.y = -outward_normal->y;
-        hit_record->normal.z = -outward_normal->z;
-    }
-}
 
 #endif
